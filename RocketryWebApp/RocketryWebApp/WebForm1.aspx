9609677785aee523d4f3206487b1d92705c6329b
AveryPratt <apratt2@uoregon.edu>
2016-05-03 22:28:36 -0700

    Added files via upload

---
+++ RocketryWebApp/RocketryWebApp/WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RocketryWebApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        How many stages is your rocket?<br />
+        <asp:TextBox ID="StageNumberTextBox" runat="server" MaxLength="2" OnTextChanged="StageNumberTextBox_TextChanged" Width="30px"></asp:TextBox>
+    
+    </div>
+        <asp:CheckBox ID="DeltaVCheckBox" runat="server" OnCheckedChanged="DeltaVCheckBox_CheckedChanged" Text="Find ΔV" />
+        <p>
+            <asp:CheckBox ID="TWRCheckBox" runat="server" AutoPostBack="True" OnCheckedChanged="TWRCheckBox_CheckedChanged" Text="Find TWR" />
+        </p>
+        <asp:DropDownList ID="ParentBodyDropDownList" runat="server" Enabled="False" Height="16px" OnSelectedIndexChanged="ParentBodyDropDownList_SelectedIndexChanged">
+            <asp:ListItem Value="1">Kerbol</asp:ListItem>
+            <asp:ListItem Value="2">Moho</asp:ListItem>
+            <asp:ListItem Value="3">Eve</asp:ListItem>
+            <asp:ListItem Value="4">Gilly</asp:ListItem>
+            <asp:ListItem Selected="True" Value="5">Kerbin</asp:ListItem>
+            <asp:ListItem Value="6">Mun</asp:ListItem>
+            <asp:ListItem Value="7">Minmus</asp:ListItem>
+            <asp:ListItem Value="8">Duna</asp:ListItem>
+            <asp:ListItem Value="9">Ike</asp:ListItem>
+            <asp:ListItem Value="10">Dres</asp:ListItem>
+            <asp:ListItem Value="11">Jool</asp:ListItem>
+            <asp:ListItem Value="12">Laythe</asp:ListItem>
+            <asp:ListItem Value="13">Vall</asp:ListItem>
+            <asp:ListItem Value="14">Tylo</asp:ListItem>
+            <asp:ListItem Value="15">Bop</asp:ListItem>
+            <asp:ListItem Value="16">Pol</asp:ListItem>
+            <asp:ListItem Value="17">Eeloo</asp:ListItem>
+        </asp:DropDownList>
+        <p>
+            <asp:Button ID="SetStagesButton" runat="server" OnClick="SetStagesButton_Clicked" Text="Create Rocket" />
+        </p>
+        <asp:Table ID="RocketTable" runat="server" Visible="False">
+            <asp:TableHeaderRow>
+                <asp:TableHeaderCell Text="My Rocket:" />
+                <asp:TableCell runat="server" Text="Δv" Visible="False" />
+                <asp:TableCell runat="server" Text="TWR" Visible="False" />
+            </asp:TableHeaderRow>
+        </asp:Table>
+    </form>
+</body>
+</html>
---
+++ RocketryWebApp/RocketryWebApp/WebForm1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RocketryWebApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        int stageNumber;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                stageNumber = 0;
+            }
+
+            bool isStageNumberInt = int.TryParse(StageNumberTextBox.Text, out stageNumber);
+
+            Response.Write("Welcome to Rocket Planner");
+
+            // Enables Parent Body drop down list only when TWR check box is checked
+            if(TWRCheckBox.Checked == true)
+            {
+                ParentBodyDropDownList.Enabled = true;
+            }
+            else
+            {
+                ParentBodyDropDownList.Enabled = false;
+            }
+        }
+
+        protected void StageNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DeltaVCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TWRCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ParentBodyDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SetStagesButton_Clicked(object sender, EventArgs e)
+        {
+            RocketTable.Visible = true;
+
+            for (int i = 0; i < stageNumber; i++)
+            {
+                RocketTable.Rows.Add(new TableRow());
+            }
+
+            foreach (TableRow myStage in RocketTable.Rows)
+            {
+                if(RocketTable.Rows.GetRowIndex(myStage).ToString() == "0")
+                {
+                    continue;
+                }
+                TableCell stageNameCell = new TableCell();
+                TableCell deltaVCell = new TableCell();
+                TableCell TWRCell = new TableCell();
+
+                myStage.Cells.Add(stageNameCell);
+                stageNameCell.Text = "Stage " + RocketTable.Rows.GetRowIndex(myStage).ToString();
+
+                if (DeltaVCheckBox.Checked)
+                {
+                    myStage.Cells.Add(deltaVCell);
+                    deltaVCell.Text = "Δv";
+                }
+
+                if (TWRCheckBox.Checked)
+                {
+                    myStage.Cells.Add(TWRCell);
+                    TWRCell.Text = "TWR";
+                }
+            }
+        }
+    }
+}
